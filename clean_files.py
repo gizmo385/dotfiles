47cfc9589b3f27ee401c4203cd4b7ab177577435
Christopher(gizmo385) <cachapline8@gmail.com>
2014-05-22 19:53:04 -0700

    Added Python script to remove unwanted vim files

---
+++ clean_files.py
@@ -0,0 +1,26 @@
+import os.path, os
+import re
+import sys
+import shlex
+
+# Get the system arguments
+args = sys.argv
+calling_dir = args[1]
+removal_count = 0
+
+files = [f for f in os.listdir(calling_dir) if os.path.isfile(f)]
+
+# This regular expression matches strings that end in a un~ or swp extension.
+p = re.compile(r'.*\.(un~|swp)')
+
+
+# Remove files whose names match the regular expression
+for file in files:
+    if p.match(file):
+        removal_count += 1
+        os.remove(file)
+
+message = "Removed " + str(removal_count) + " "
+message += "files." if removal_count >= 2 else "file."
+
+print( message )
