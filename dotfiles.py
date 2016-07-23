9c0d3a822efa3153a3805e90fc573386b88c4e9c
Christopher Chapline <cachapline8@gmail.com>
2016-07-23 12:42:45 -0600

    Hard implementations of all functions, test config

--- dotfiles.py
+++ dotfiles.py
@@ -1,24 +1,32 @@
 from __future__ import print_function
 
 from distutils import spawn # For find_executable
+from datetime import datetime # For timestamps
+from os import path # For path.abspath
+import shutil # For copy
+import errno
 import json # To parse the input
 import subprocess # To execute shell commands
 import sys # To exit the program with an exit status
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
+log_file = "dotfiles_log"
+
 def execute_shell_command(command, *args):
-    finished_args = command.split(" ")
-    finished_args.extend(flatten(map(lambda arg: arg.split(" "), args)))
-    finished_args = finished_args
+    finished_command = command.split(" ")
+    finished_command.extend(flatten(map(lambda arg: arg.split(" "), args)))
 
-    print("Command:", " ".join(finished_args))
-    return 0
+    with open(log_file, "a") as output:
+        output.write(" ".join(finished_command) + "\n")
+        # return subprocess.call(["echo", "Command:"] + finished_command,
+                               # stdout=output, stderr=output)
+        return subprocess.call(finished_command, stdout=output, stderr=output)
 
-    # return subprocess.call(finished_args)
 
 def command_exists(command_to_check):
     return spawn.find_executable(command_to_check) is not None
+
 
 def get_package_sources(config):
     """
@@ -45,36 +53,59 @@ def install_packages(config, package_sources):
                 print("Error while installing package:", package)
 
 
+def copy(src, dst):
+    src = path.abspath(src)
+    src = path.abspath(src)
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc:
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else:
+            raise
+
 def copy_dotfiles(config):
     """Copies the dotfiles into the installation directory"""
-    install_dir = config["install_dir"]
-    copy_message= "\tCopying {name} to {install_dir}."
+    dest = path.abspath(config["install_dir"])
+    src = path.abspath(config["dotfiles_folder"])
+
+    copy_message= "\tCopying {src}/{name} into to {dest}."
 
     dotfile_groups = config["dotfiles"]
     for group in dotfile_groups:
         print("Copying {group} dotfiles".format(group=group))
 
         for dotfile in dotfile_groups[group]:
-            print(copy_message.format(name=dotfile, install_dir=install_dir))
+            copy("{src}/{name}".format(src=src, name=dotfile),
+                 "{dest}/{name}".format(dest=dest, name=dotfile))
+            print(copy_message.format(src=src, name=dotfile, dest=dest))
 
 
 def create_links(config):
     if "link_dir" not in config:
         return
 
+    install = path.abspath(config["install_dir"])
+    link = path.abspath(config["link_dir"])
+
+    # String formats
+    success = "\tCreated link: {src}/{name} --> {dest}/{name}"
+    error = "\tError creating link: {src}/{name} --> {dest}/{name}"
+    command = "ln -s {src}/{name} {dest}/{name}"
+
     print("Creating symbolic links:")
-
-    install_dir = config["install_dir"]
-    link_dir = config["link_dir"]
-
-    message = "\t{src}/{name} --> {dest}/{name}"
-
     dotfile_groups = config["dotfiles"]
     for group in dotfile_groups:
         for dotfile in dotfile_groups[group]:
-            print(message.format(src=install_dir, dest=link_dir, name=dotfile))
+            # Execute the commmand
+            status = execute_shell_command(command.format(src=install,
+                                                          dest=link,
+                                                          name=dotfile))
 
-    pass
+            if status != 0:
+                print(error.format(src=install, dest=link, name=dotfile))
+            else:
+                print(success.format(src=install, dest=link, name=dotfile))
 
 
 def execute_commands(config):
@@ -83,8 +114,7 @@ def execute_commands(config):
 
     for command in custom_commands:
         print(command["message"])
-        print("\t", command["command"])
-    pass
+        execute_shell_command(command["command"])
 
 
 def install_dotfiles(config):
@@ -92,12 +122,24 @@ def install_dotfiles(config):
         print("An installation directory is required!")
         sys.exit(1)
 
+    if "dotfiles_folder" not in config:
+        print("You must provide the location of the dotfiles to install!")
+        sys.exit(1)
+
+    print("Writing command output to", log_file)
+
+    with open(log_file, "a") as f:
+        f.write("Starting install at: {time}\n".format(time=datetime.now()))
+
     # Run through the installation
     package_sources = get_package_sources(config)
     install_packages(config, package_sources)
     copy_dotfiles(config)
     create_links(config)
     execute_commands(config)
+
+    with open(log_file, "a") as f:
+        f.write("Finishing install at: {time}\n".format(time=datetime.now()))
 
 
 with open("dotfiles.json") as f:
