cc18f14bb7c756c76de53427915709c71b9a0262
Christopher Chapline <cachapline8@gmail.com>
2021-05-08 22:12:31 -0700

    Mosh/tmux/skhd/chunkwm & config cleanup

--- dotfiles/.nixpkgs/darwin-configuration.nix
+++ dotfiles/.nixpkgs/darwin-configuration.nix
@@ -1,42 +1,48 @@
 { config, pkgs, ... }:
 
-{
+  {
   # List packages installed in system profile. To search by name, run:
   environment.systemPackages =
     [
-        pkgs.vim
-        pkgs.fira-code
-        pkgs.jq
-        pkgs.ripgrep
-        pkgs.docker
-        pkgs.nodejs
-        pkgs.python38
-        pkgs.clojure
-        pkgs.skhd
-        pkgs.chunkwm
-        pkgs.neovim
+      pkgs.awscli
+      pkgs.chunkwm
+      pkgs.clojure
+      pkgs.docker
+      pkgs.fira-code
+      pkgs.htop
+      pkgs.jq
+      pkgs.mosh
+      pkgs.neovim
+      pkgs.nodejs
+      pkgs.ripgrep
+      pkgs.skhd
+      pkgs.tmux
+      pkgs.vim
     ];
 
-  nixpkgs.overlays = [
-   (self: super: {
-     neovim = super.neovim.override {
-       viAlias = true;
-       vimAlias = true;
-     };
-   })
- ];
+    nixpkgs.overlays = [
+      (self: super: {
+        neovim = super.neovim.override {
+          viAlias = true;
+          vimAlias = true;
+        };
+      })
+    ];
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
  # Fonts
-  fonts = {
-    enableFontDir = true;
-    fonts = [ pkgs.fira-code ];
-  };
+ fonts = {
+   enableFontDir = true;
+   fonts = [ pkgs.fira-code ];
+ };
 
   # Shell
   programs.bash.enable = false;
   programs.zsh.enable = true;
+
+  # Enable/Disable services
+  services.skhd.enable = true;
 }
