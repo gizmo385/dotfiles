1bef7a68b2ca63611358b7e12585082221bec550
Christopher Chapline <cachapline8@gmail.com>
2021-08-19 11:15:44 -0600

    Getting the nix-env command to work in coder personalization

--- coder_personalize.sh
+++ coder_personalize.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 echo "Installing nix packages"
-nix-env -iA \
-    nixpkgs.bat \
-    nixpkgs.fzf \
-    nixpkgs.gitAndTools.delta \
-    nixpkgs.neovim \
-    nixpkgs.nodePackages.pyright \
-    nixpkgs.ripgrep \
-    nixpkgs.tmux
+
+if [[ -d "$HOME/.nix-profile/bin/nix-env" ]]; then
+    echo "Installing nix packages"
+    $HOME/.nix-profile/bin/nix-env -iA \
+        nixpkgs.bat \
+        nixpkgs.fzf \
+        nixpkgs.gitAndTools.delta \
+        nixpkgs.neovim \
+        nixpkgs.nodePackages.pyright \
+        nixpkgs.ripgrep \
+        nixpkgs.tmux
+fi
 
 echo "Changing shell to zsh"
 sudo chsh -s $(which zsh) $USER
