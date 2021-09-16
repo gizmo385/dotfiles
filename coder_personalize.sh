b55a4297901f78bb7baec019cfff7f294dd076c7
Christopher Chapline <cachapline8@gmail.com>
2021-09-16 17:20:51 -0600

    Install neovim from apt-get for coder

--- coder_personalize.sh
+++ coder_personalize.sh
@@ -4,5 +4,8 @@ if [[ -f $NIX_BIN_LOCATION/nix-env ]]; then
     echo "Installing nix packages"
     $NIX_BIN_LOCATION/nix-channel --update
     $NIX_BIN_LOCATION/nix-env --upgrade
-    $NIX_BIN_LOCATION/nix-env -iA nixpkgs.bat nixpkgs.fzf nixpkgs.gitAndTools.delta nixpkgs.neovim nixpkgs.nodePackages.pyright nixpkgs.ripgrep nixpkgs.tmux
+    $NIX_BIN_LOCATION/nix-env -iA nixpkgs.bat nixpkgs.fzf nixpkgs.gitAndTools.delta nixpkgs.nodePackages.pyright nixpkgs.ripgrep nixpkgs.tmux
+    sudo add-apt-repository ppa:neovim-ppa/unstable
+    sudo apt-get update
+    sudo apt-get install neovim
 fi
