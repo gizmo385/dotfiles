2aa1c72c7dc2eaadac11d3b9ca961930723d9da2
Christopher Chapline <cachapline8@gmail.com>
2021-09-17 15:13:13 -0600

    Faster git prompt for fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -5,4 +5,9 @@ if status is-interactive
     end
 
     source $HOME/.bash_aliases
+
+    # Faster git prompt, only show the branch
+    function fish_git_prompt
+        set_color yellow; printf '[%s] ' (git branch --show-current); set_color normal;
+    end
 end
