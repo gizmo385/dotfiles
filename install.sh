2bf9a3f21725da1de378eb0928d5c3fc8c49c968
Chris <cachapline8@gmail.com>
2023-09-15 02:08:54 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,10 +57,15 @@ $NIX_BIN/nix-env --show-trace -i -f "$HOME/.nixpkgs/dev-env.nix"
 
 # Install neovim on Linux
 if [[ $OSTYPE == 'linux'* ]]; then
-
+    if grep -sq 'docker\|lxc' /proc/1/cgroup; then
+        add-apt-repository -y ppa:neovim-ppa/unstable
+        apt-get update -y
+        apt-get install -y neovim
+    else
         sudo add-apt-repository -y ppa:neovim-ppa/unstable
         sudo apt-get update -y
         sudo apt-get install -y neovim
+    fi
 fi
 
 
