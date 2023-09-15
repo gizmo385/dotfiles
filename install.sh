92d47038e91b02d2c3b0f323071dbaedc6c42685
Chris <cachapline8@gmail.com>
2023-09-15 01:42:01 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,15 +57,15 @@ $NIX_BIN/nix-env --show-trace -i -f "$HOME/.nixpkgs/dev-env.nix"
 
 # Install neovim on Linux
 if [[ $OSTYPE == 'linux'* ]]; then
-    if [ "$EUID" -ne 0 ];
+    if [ -f /.dockerenv ];
     then
-        sudo add-apt-repository -y ppa:neovim-ppa/unstable
-        sudo apt-get update -y
-        sudo apt-get install -y neovim
-    else
         add-apt-repository -y ppa:neovim-ppa/unstable
         apt-get update -y
         apt-get install -y neovim
+    else
+        sudo add-apt-repository -y ppa:neovim-ppa/unstable
+        sudo apt-get update -y
+        sudo apt-get install -y neovim
     fi
 
 fi
