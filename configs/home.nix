e92b7b8bfd236c1870c9d0bfee15f230e55ccd3a
gizmo385 <gizmo385@users.noreply.github.com>
2024-05-30 05:52:47 +0000

    Install some missing tooling

--- configs/home.nix
+++ configs/home.nix
@@ -36,12 +36,24 @@ in
     };
   };
 
+  home.packages = [
+    # Tool for managing k8s contexts and namespaces
+    pkgs.kubectx
+    # Alternative to man pages that provides examples
+    pkgs.tldr
+    # Tool for parsing YAML output (jq but for YAML)
+    pkgs.yq
+  ];
+
   programs = {
     home-manager.enable = true;
 
     zsh = import ./nix/zsh.nix { inherit homeDirectory pkgs; };
     git = import ./nix/git.nix { inherit pkgs; };
     nixvim = import ./nix/neovim;
+
+    # Tool for parsing JSON output
+    jq.enable = true;
 
     # Better shell tooling
     bat.enable = true; # Alternative to cat
