7ccc7c612f2f6c2936994ea1e17e2d1b73f5d48e
gizmo385 <gizmo385@users.noreply.github.com>
2024-06-06 22:43:11 +0000

    Add wiki section to alpha dash

--- modules/neovim/plugins/alpha.nix
+++ modules/neovim/plugins/alpha.nix
@@ -100,13 +100,34 @@ in
                     position = "center";
                   };
                   type = "text";
+                  val = "Wiki";
+                }
+                (mkButton "w" ":VimwikiIndex<CR>" " > Open Wiki")
+                (mkButton "d" ":VimwikiDiaryIndex<cr>:VimwikiDiaryGenerateLinks<cr>" " > Open Diary")
+                (mkButton "n" ":VimwikiMakeDiaryNote<cr>" "✎ > New Diary Entry")
+                (mkButton "s" "<CMD>lua require('telescope').extensions.vimwiki.live_grep()<cr>" " > Search Wiki")
+              ];
+            }
+            {
+              type = "padding";
+              val = 2;
+            }
+            {
+              type = "group";
+              val = [
+                {
+                  opts = {
+                    hl = "Keyword";
+                    position = "center";
+                  };
+                  type = "text";
                   val = "Commands";
                 }
-              (mkButton "e" ":ene<CR>" "  New file")
-              (mkButton "f" "<CMD>lua require('telescope.builtin').find_files()<CR>" "  Find File")
-              (mkButton "/" "<CMD>lua require('telescope.builtin').live_grep()<CR>" "  Search")
-              (mkButton "r" "<CMD>lua require('telescope.builtin').oldfiles()<CR>" "  Recent")
-              (mkButton "q" ":qa<CR>" "  Quit")
+              (mkButton "e" ":ene<CR>" "  > New file")
+              (mkButton "f" "<CMD>lua require('telescope.builtin').find_files()<CR>" "  > Find File")
+              (mkButton "/" "<CMD>lua require('telescope.builtin').live_grep()<CR>" "  > Search")
+              (mkButton "r" "<CMD>lua require('telescope.builtin').oldfiles()<CR>" "  > Recent")
+              (mkButton "q" ":qa<CR>" "  > Quit")
             ];
           }
           {
