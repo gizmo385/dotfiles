5567ba5a039cbb56c45ba50f55a0f384c9cd6923
gizmo385 <gizmo385@users.noreply.github.com>
2024-11-25 20:33:20 +0000

    Set termcolor for lazy-github, remove old configs

--- modules/lazy_github.nix
+++ modules/lazy_github.nix
@@ -1,62 +1,20 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, ... }:
 
 let 
-  inherit (lib) mkIf mkOption types;
-  inherit (config.gizmo) lazy-github;
   python-bin = "${pkgs.python311}/bin/python";
   uvx-bin = "${pkgs.uv}/bin/uvx";
-  lazy-github-command = "${uvx-bin} --quiet --python ${python-bin} lazy-github";
+  lazy-github-command = "TERM=xterm-256color ${uvx-bin} --quiet --python ${python-bin} lazy-github";
 in
   {
-  options.gizmo.lazy-github = {
-    enable = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Whether or not to enable lazy-github";
-    };
-    addNeovimBinding = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Whether or not to setup a float term binding";
-    };
-    appearance.dark_mode = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Whether or not to use dark mode";
-    };
-    repositories.favorites = mkOption {
-      type = types.listOf types.str;
-      default = [
-        "gizmo385/lazy-github"
-        "gizmo385/dotfiles"
-      ];
-      description = "Repositories to pin at the top";
-    };
-    pull_requests.state_filter = mkOption {
-      type = types.enum [ "all" "open" "closed" ];
-      default = "all";
-      description = "What pull requests to display by default";
-    };
-    api.base_url = mkOption {
-      type = types.str;
-      default = "https://api.github.com";
-      description = "The API used to pull GitHub information from";
-    };
-    cache.repo_cache_duration = mkOption {
-      type = types.int; 
-      default = 3600;
-      description = "Number of seconds that repository information should be cached for";
-    };
-  };
 
   # TODO: I'd like to set up an actual derivation for this and pull this out as a separate script,
   # but this'll do for now
   config = {
-    programs.zsh.shellAliases = mkIf lazy-github.enable {
+    programs.zsh.shellAliases =  {
       lh = lazy-github-command;
       lazy-github = lazy-github-command;
     };
-    programs.nixvim.keymaps = mkIf lazy-github.addNeovimBinding [{
+    programs.nixvim.keymaps =  [{
       key = "<leader>G";
       action = ":FloatermNew --width=0.9 --height=0.95 ${lazy-github-command}<CR>";
       mode = "n";
