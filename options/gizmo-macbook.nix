5cc688e555050a1572fbfbbd1a33ec32c7dca3e5
gizmo385 <gizmo385@users.noreply.github.com>
2024-12-01 02:46:37 -0700

    Adjust gizmo-macbook settings

--- options/gizmo-macbook.nix
+++ options/gizmo-macbook.nix
@@ -3,9 +3,13 @@
 {
   config.gizmo = {
     username = "gizmo385";
-    languages.rust = {
-      toolchain = true;  
-      lsp = true;  
+    languages = {
+      rust = {
+        toolchain = true;  
+        lsp = true;  
+      };
+      python.linters.pylsp = false;
     };
+
   };
 }
