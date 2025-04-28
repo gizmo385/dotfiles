b06899b0339e4a5c4d6eed8ab998a63d6c6c4e18
gizmo385 <gizmo385@users.noreply.github.com>
2025-04-28 16:32:22 +0000

    Fix deprecated zsh.initExtra, swap to code companion

--- modules/work.nix
+++ modules/work.nix
@@ -12,16 +12,8 @@ let
       rev = "ff6d2ad447d17f5c7eaf64fb99a77d1afaa02eda";
     };
   };
-  
-  claude-code = pkgs.vimUtils.buildVimPlugin {
-    name = "greggh/claude-code.nvim";
-    src = builtins.fetchGit {
-      url = "git@github.com:greggh/claude-code.nvim.git";
-      rev = "d1dbc6b7025c4f034e14cc0dda6d29d5a6a5c4e8";
-    };
-  };
 in
-{
+  {
   config = mkIf work {
     home = {
       packages = [
@@ -40,68 +32,89 @@ in
       lazygit.settings = {
         git.branchPrefix = "gizmo/";
       };
-    };
 
 
-    # Disable oh-my-zsh git prompt statuses because they're slow on the monorepo
-    programs.git.extraConfig = {
-      oh-my-zsh = {
-        hide-status = 1;
-        hide-dirty = 1;
+      zsh.initContent = ''
+      if [ -e $HOME/.anthropic_api_key ]
+      then
+          export ANTHROPIC_API_KEY="$(cat $HOME/.anthropic_api_key)"
+      fi
+      '';
+
+      # Disable oh-my-zsh git prompt statuses because they're slow on the monorepo
+      git.extraConfig = {
+        oh-my-zsh = {
+          hide-status = 1;
+          hide-dirty = 1;
+        };
+      };
+
+      nixvim = {
+        # Enable some LSPs for I only care about at work
+        plugins = {
+          codecompanion = {
+            enable = true;
+            settings = {
+              strategies = {
+                agent = {
+                  adapter = "anthropic";
+                };
+                chat = {
+                  adapter = "anthropic";
+                };
+                inline = {
+                  adapter = "anthropic";
+                };
+              };
+            };
+          };
+
+          lsp.servers = {
+            bashls.enable = true;
+            terraformls.enable = true;
+            dockerls.enable = true;
+            docker_compose_language_service.enable = true;
+            starlark_rust.enable = true;
+            elixirls.enable = true;
+          };
+        };
+
+        keymaps = [
+          {
+            key = "<leader>co";
+            action = ":CodeownersWhoOwns<CR>";
+            mode = "n";
+          }
+          {
+            key = "<leader>ct";
+            action = ":CodeownersGotoTeamDefinition<CR>";
+            mode = "n";
+          }
+          {
+            key = "<leader>co";
+            action = ":CodeownersGotoTeamHelpChannel<CR>";
+            mode = "n";
+          }
+          {
+            key = "<leader>C";
+            action = ":FloatermNew --width=0.9 --height=0.9 clyde tui<CR>";
+            mode = "n";
+          }
+
+          {
+            key = "<leader>aa";
+            action = ":CodeCompanionChat Toggle<CR>";
+            mode = "n";
+          }
+
+        ];
       };
     };
+
 
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
       "claude-code"
     ];
 
-    programs.nixvim = {
-      # Enable some LSPs for I only care about at work
-      plugins = {
-        lsp.servers = {
-          bashls.enable = true;
-          terraformls.enable = true;
-          dockerls.enable = true;
-          docker_compose_language_service.enable = true;
-          elixirls.enable = true;
-        };
-      };
-
-      extraConfigLuaPost = ''
-      require('claude-code').setup()
-      '';
-
-      extraPlugins = [ codeowners claude-code ];
-
-      keymaps = [
-        {
-          key = "<leader>co";
-          action = ":CodeownersWhoOwns<CR>";
-          mode = "n";
-        }
-        {
-          key = "<leader>ct";
-          action = ":CodeownersGotoTeamDefinition<CR>";
-          mode = "n";
-        }
-        {
-          key = "<leader>co";
-          action = ":CodeownersGotoTeamHelpChannel<CR>";
-          mode = "n";
-        }
-        {
-          key = "<leader>C";
-          action = ":FloatermNew --width=0.9 --height=0.9 clyde tui<CR>";
-          mode = "n";
-        }
-        
-        {
-          key = "<leader>aa";
-          action = ":ClaudeCode<CR>";
-          mode = "n";
-        }
-
-      ];
-    };
   };
 }
