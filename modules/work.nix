4f176acd0c7a4b4d8ca2f6d407a613219495d3c9
gizmo385 <gizmo385@users.noreply.github.com>
2025-06-11 16:32:03 +0000

    Remove codeowners plugin
    It doesn't support lualine currently

--- modules/work.nix
+++ modules/work.nix
@@ -1,22 +1,12 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }:
 
 let
   inherit (lib) mkIf;
   inherit (config.gizmo) work;
-
-  # Custom codeownership plugin
-  codeowners = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-codeowners";
-    src = builtins.fetchGit {
-      url = "git@github.com:discord/vim-codeowners.git";
-      rev = "ff6d2ad447d17f5c7eaf64fb99a77d1afaa02eda";
-    };
-  };
 in
 {
   config = mkIf work {
@@ -52,7 +42,6 @@ in
       };
 
       nixvim = {
-        extraPlugins = [ codeowners ];
         # Enable some LSPs for I only care about at work
         plugins = {
           lsp.servers = {
@@ -80,21 +69,6 @@ in
         ];
 
         keymaps = [
-          {
-            key = "<leader>co";
-            action = ":CodeownersWhoOwns<CR>";
-            mode = "n";
-          }
-          {
-            key = "<leader>ct";
-            action = ":CodeownersGotoTeamDefinition<CR>";
-            mode = "n";
-          }
-          {
-            key = "<leader>co";
-            action = ":CodeownersGotoTeamHelpChannel<CR>";
-            mode = "n";
-          }
           {
             key = "<leader>C";
             action = ":FloatermNew --width=0.9 --height=0.9 clyde tui<CR>";
