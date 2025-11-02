9758e72606b07134d82f52ce36b5b66c962bafda
gizmo385 <gizmo385@users.noreply.github.com>
2025-11-02 01:56:09 -0700

    Switch quicklinks config to use github version of plugin

--- modules/neovim/plugins/quicklinks.nix
+++ modules/neovim/plugins/quicklinks.nix
@@ -1,21 +1,26 @@
 { pkgs, ... }:
 
+let
+  nvim-quicklinks = pkgs.vimUtils.buildVimPlugin {
+    pname = "nvim-quicklinks";
+    version = "latest";
+    src = pkgs.fetchFromGitHub {
+      owner = "gizmo385";
+      repo = "nvim-quicklinks";
+      rev = "103dccb9dcf608d24770f4029eb7b7fea2b876e3";
+      hash = "sha256-Zo0ERf/HDnU5CvFpk7eO2DyKB8q7s4CybgtNyO+lv+s=";
+    };
+  };
+in
 {
-  extraConfigLua = ''
-    -- Add nvim-quicklinks to runtime path if it exists locally
-    local quicklinks_path = vim.fn.expand('~/workspace/nvim-quicklinks')
-    if vim.fn.isdirectory(quicklinks_path) == 1 then
-      vim.opt.runtimepath:append(quicklinks_path)
+  extraPlugins = [ nvim-quicklinks ];
 
-      -- Setup quicklinks if successfully loaded
-      local ok, quicklinks = pcall(require, 'quicklinks')
-      if ok then
-        quicklinks.setup({
-          debug = false,
-          enable_project_config = true,
-        })
-      end
-    end
+  extraConfigLua = ''
+    -- Setup quicklinks
+    require('quicklinks').setup({
+      debug = false,
+      enable_project_config = true,
+    })
   '';
 
   keymaps = [
