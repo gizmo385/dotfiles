bb41dd65a3dac557db345f935eb17dffa62a13fc
gizmo385 <gizmo385@users.noreply.github.com>
2026-05-03 03:04:16 -0600

    Set BROWSER to windows explorer in WSL
    This allows code like the python webbrowser module to properly open
    external links.

--- options/wsl.nix
+++ options/wsl.nix
@@ -1,14 +1,20 @@
 { ... }:
 
 {
-  config.gizmo = {
-    username = "gizmo";
+  config = {
+    gizmo = {
+      username = "gizmo";
 
-    languages = {
-      rust = {
-        toolchain = true;
-        lsp = true;
+      languages = {
+        rust = {
+          toolchain = true;
+          lsp = true;
+        };
       };
     };
+
+    # wslu (which provides wslview) isn't packaged in nixpkgs, so point xdg-open
+    # consumers at Windows' explorer.exe to handle URL launches from WSL.
+    home.sessionVariables.BROWSER = "/mnt/c/Windows/explorer.exe";
   };
 }
